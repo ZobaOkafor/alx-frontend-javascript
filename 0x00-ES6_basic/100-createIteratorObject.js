d27b33cf37ac22376767917ad54126771c1e1a0e
ZobaOkafor <judithokafor30@gmail.com>
2024-06-25 00:21:28 +0000

    ES6 basic

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,15 @@
+export default function createIteratorObject(report) {
+  const departments = Object.values(report.allEmployees);
+
+  function* employeeIterator() {
+    for (const employees of departments) {
+      for (const employee of employees) {
+        yield employee;
+      }
+    }
+  }
+
+  return {
+    [Symbol.iterator]: employeeIterator
+  };
+}
