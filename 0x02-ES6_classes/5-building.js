5c3539814bd2250a11c01b2be25bbe950fe13a56
ZobaOkafor <judithokafor30@gmail.com>
2024-06-27 19:19:19 +0000

    ES6 classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = this.validateNumber(sqft, 'Square feet');
+  }
+
+  // Validator
+  validateNumber(value, attributeName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${attributeName} must be a number`);
+    }
+    return value;
+  }
+
+  // Getter
+  get sqft() {
+    return this._sqft;
+  }
+}
