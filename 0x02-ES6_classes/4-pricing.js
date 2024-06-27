3b3fa94f19a6dbe1d675014baf05ebdf43927c4d
ZobaOkafor <judithokafor30@gmail.com>
2024-06-27 19:13:38 +0000

    ES6 classes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,54 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = this.validateNumber(amount, 'Amount');
+    this._currency = this.validateCurrency(currency, 'Currency');
+  }
+
+  // Validators
+  validateNumber(value, attributeName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${attributeName} must be a number`);
+    }
+    return value;
+  }
+
+  validateCurrency(value, attributeName) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError(`${attributeName} must be an instance of Currency`);
+    }
+    return value;
+  }
+
+  // Getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // Setters
+  set amount(value) {
+    this._amount = this.validateNumber(value, 'Amount');
+  }
+
+  set currency(value) {
+    this._currency = this.validateCurrency(value, 'Currency');
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
