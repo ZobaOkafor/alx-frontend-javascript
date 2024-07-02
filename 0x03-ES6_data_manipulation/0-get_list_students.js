b260a1832db79edd48ccc9eb7cc582bfc60664b7
ZobaOkafor <judithokafor30@gmail.com>
2024-07-02 10:42:56 +0000

    ES6 Data Manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Returns an array of student objects.
+ * Each object contains the attributes: id (Number), firstName (String), and location (String).
+ * @returns {Array} Array of student objects.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
