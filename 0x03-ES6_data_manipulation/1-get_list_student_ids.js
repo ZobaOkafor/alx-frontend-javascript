0887704bcd4ec31538540b12166289f46938be65
ZobaOkafor <judithokafor30@gmail.com>
2024-07-02 10:50:56 +0000

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -5,7 +5,8 @@
  * @returns {Array} Array of student IDs.
  */
 export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
+  if (Array.isArray(students)) {
+    return students.map((student) => student.id);
   }
-  return students.map(student => student.id);
+  return [];
+}
