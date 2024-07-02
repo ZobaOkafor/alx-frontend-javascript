019d8c668c26fb0775e63e44d8df2faad07a97af
ZobaOkafor <judithokafor30@gmail.com>
2024-07-02 12:57:01 +0000

    ES6 Data Manipulation

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * Returns a Map of groceries with specified items and quantities.
+ * @returns {Map} A Map containing groceries with items as keys and quantities as values.
+ */
+export default function groceriesList() {
+  const groceries = new Map();
+  
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+  
+  return groceries;
+}
