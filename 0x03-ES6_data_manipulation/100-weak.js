8a76377c5d85934ec74c26fbd08c08cc242a68a6
ZobaOkafor <judithokafor30@gmail.com>
2024-07-02 14:41:11 +0000

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,18 @@
-// Define a new WeakMap instance
-export const weakMap = new WeakMap();
-
 /**
  * Queries an API endpoint and tracks the number of times it is called for each endpoint.
  * Throws an error if the number of queries is >= 5 for a given endpoint.
  * @param {Object} endpoint - The endpoint object containing protocol and name properties.
  * @throws {Error} If the number of queries for the endpoint is >= 5.
  */
+// Define a new WeakMap instance
+export const weakMap = new WeakMap();
+let i = 1;
+
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+  weakMap.set(endpoint, i);
+  i += 1;
+  const query = weakMap.get(endpoint);
+  if (query >= 5) {
+    throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
 }
