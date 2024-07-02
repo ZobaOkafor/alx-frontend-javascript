39449074fe6bb00b3c4e5dc2c0ad9371c02102de
ZobaOkafor <judithokafor30@gmail.com>
2024-07-02 12:58:16 +0000

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -5,5 +5,5 @@
  * @returns {boolean} True if all elements in the array exist in the set, false otherwise.
  */
 export default function hasValuesFromArray(set, array) {
-  return array.every(element => set.has(element));
+  return array.every((element) => set.has(element));
 }
